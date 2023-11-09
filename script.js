00b20ae7955ade67079497e651082917e7184c44
Nikitabc <tretjakov.nikitabc@gmail.com>
2023-11-09 08:08:01 +0300

    5commitfinal

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * 0.02}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -0.02}deg;
+        `
+    })
+})
+
+function redirect() {
+    // Здесь укажите URL, на который вы хотите перейти
+    var url = "pages/theory.html";
+  
+    // Осуществляем переход по URL
+    window.location.href = url;
+  }
+
+
+
+
+
+
+
+  const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      // удалим у кнопки класс btn-up_hide
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      // добавим к кнопке класс btn-up_hide
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      // при прокрутке содержимого страницы
+      window.addEventListener('scroll', () => {
+        // определяем величину прокрутки
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      // при нажатии на кнопку .btn-up
+      document.querySelector('.btn-up').onclick = () => {
+        // переместим в начало страницы
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
